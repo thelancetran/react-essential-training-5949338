bf551c4e21210671a2c1be9807f6cb31c698189d
Lance Tran <thelancetran@gmail.com>
2025-04-06 01:00:48 -0700

    Updated App.jsx to show App component rendering Header component. Also showing wrapping with enclosing tag.

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 
-let language = "React";
-let moon = "🌙";
+// JSX tags - think like HTML tags to wrap content
+function Header() {
+  return (
+    <header>
+      <h1>Eve's Kitchen</h1>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <h1>
-      Hello {language.toUpperCase()} {moon}!!
-    </h1>
+    <div>
+      <Header />
+      <main>
+        <h2>We serve the most delicious food</h2>
+      </main>
+    </div>
   );
 }
 
