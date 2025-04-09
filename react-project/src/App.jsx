150fdb15a4a1897b27604796bc16144791db0270
Lance Tran <thelancetran@gmail.com>
2025-04-08 21:56:31 -0700

    Updated App.jsx. Passed list as component property, then used .map method to loop through each element to construct a list

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -9,13 +9,27 @@ function Header({ name, year }) {
   );
 }
 
+const items = [
+  "Cheese",
+  "Hot Dog",
+  "Eggs"
+];
+
+function Main({  dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{listStyleType: "none"}}>{dish}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Main dishes={items}/>
     </div>
   );
 }
